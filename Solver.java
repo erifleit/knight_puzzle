92c6ed39d21abee4e47124f9c1a2227987b708b9
Eric Fleith <ericfleith@gmail.com>
2018-05-29 23:41:00 -0400

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,7 +1,7 @@
 public class Solver{
    public static void main(String a[]){
 
-      Board b = new Board(9); // choosing 9 or higher will take an extremely long time 
+      Board b = new Board(8); // choosing 9 or higher will take an extremely long time 
       System.out.println(b.start());
    }
 }
